fb0a7c2e87f2fedec3f099c2423d1c50007c67a9
ifpr <nathaliadeliz3@gmail.com>
2023-02-23 12:59:00 -0300

    inicio do projeto

--- app/src/main/java/com/example/aua23_02/MainActivity.java
+++ app/src/main/java/com/example/aua23_02/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.aua23_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonIrCadastro;
+    TextView etNome;
+    TextView etEmail;
+    TextView etTelefone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonIrCadastro=findViewById(R.id.buttonIrCadastro);
+
+
+        buttonIrCadastro.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, Cadastro_activity.class);
+                in.putExtra("Nome", etNome.getText().toString());
+                in.putExtra("Email", etEmail.getText().toString());
+                in.putExtra("Telefone", etTelefone.getText().toString());
+
+                startActivity(in);
+
+            }
+        });
     }
 }
